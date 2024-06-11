16450a8a6d08589944956ec11967b659d6d2c71b
dung <dovanminhdung2001@gmail.com>
2024-06-11 23:56:47 +0700

    feat: get medical-history 4 doctor

--- project - da/src/main/java/springboot/project/controller/MedicalHistoryController.java
+++ project - da/src/main/java/springboot/project/controller/MedicalHistoryController.java
@@ -4,7 +4,9 @@ package springboot.project.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import springboot.project.entity.MedicalHistory;
 import springboot.project.model.MedicalHistoryDTO;
+import springboot.project.model.MessageResponseDTO;
 import springboot.project.service.MedicalHistoryService;
 
 @RestController
@@ -22,9 +24,22 @@ public class MedicalHistoryController {
             @RequestParam(required = false) Integer id,
             @RequestParam(required = false) Integer userId
     ) {
-        return  id != null
-                ? ResponseEntity.ok(medicalHistoryService.findById(id))
-                : ResponseEntity.ok(medicalHistoryService.findByUser_Id(userId));
+        MedicalHistory response =  (id != null)
+                ? medicalHistoryService.findById(id)
+                : medicalHistoryService.findByUser_Id(userId);
+
+        return response == null
+                ? ResponseEntity.ok(new MessageResponseDTO("User not update medical history yet"))
+                : ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/doctor/medical-history/get")
+    public ResponseEntity<?> getForDoctor(@RequestParam Integer userId) {
+        MedicalHistory res = medicalHistoryService.findByUser_Id(userId);
+
+        return res == null
+                ? ResponseEntity.ok(new MessageResponseDTO("User not update medical history yet"))
+                : ResponseEntity.ok(res);
     }
 
     @PutMapping("/user/medical-history/update")
